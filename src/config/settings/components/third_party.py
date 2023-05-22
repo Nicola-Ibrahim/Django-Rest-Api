9810d4681755a09b4b72021410b558fe8a071775
Nicola <nqola.ibrahim@lit-co.net>
2023-05-22 12:23:58 +0300

    Worked with custom jwt authentication class

--- src/config/settings/components/third_party.py
+++ src/config/settings/components/third_party.py
@@ -38,7 +38,8 @@ INSTALLED_APPS += ("rest_framework",)  # type: ignore # noqa: F821
 REST_FRAMEWORK = {
     # "DEFAULT_SCHEMA_CLASS": ("rest_framework.schemas.coreapi.AutoSchema"),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "apps.authentication.api.authentication.CustomJWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
 }
