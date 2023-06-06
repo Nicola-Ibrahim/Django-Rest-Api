4d39bd2665f8366e177cc2fa5d0390c2e7f62835
Nicola <nqola.ibrahim@lit-co.net>
2023-06-06 11:54:03 +0300

    update database configuration

--- src/config/settings/components/database.py
+++ src/config/settings/components/database.py
@@ -4,7 +4,13 @@
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "dauth",
+        "USER": "admin",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "ATOMIC_REQUESTS": True,
+        "CONN_MAX_AGE": 0,
     }
 }
