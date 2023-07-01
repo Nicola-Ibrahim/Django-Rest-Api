7122937eba7ebf80024a53757b8ab5470117ffdf
Nicola Ibrahim <nicolaibrahim969@gmail.com>
2023-07-01 11:36:21 +0300

    Added namespace attributes for the apps API

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,8 +5,12 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/accounts/", include("src.apps.accounts.api.urls"), name="accounts"),
-    path("api/auth/", include("src.apps.authentication.api.urls"), name="auth"),
+    path(
+        "api/accounts/", include("src.apps.accounts.api.urls", namespace="accounts-api")
+    ),
+    path(
+        "api/auth/", include("src.apps.authentication.api.urls", namespace="auth-api")
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
