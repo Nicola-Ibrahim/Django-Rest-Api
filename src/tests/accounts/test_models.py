c59ca88963cfb454b0c4e8dba6b5375aeeec0672
nicola <nicolaibrahim969@gmail.com>
2023-07-11 12:22:53 +0300

    Added testing accounts model

---
+++ src/tests/accounts/test_models.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.apps.accounts.models import models
+
+
+class TestUserModel:
+    def test_full_name(self, db, one_user):
+        one_user.first_name = "Django"
+        one_user.last_name = "D"
+        assert one_user.get_full_name() == "Django D"
+
+    def test_set_password(self, db, one_user):
+        one_user.set_password("Django pass")
+        assert one_user.check_password("Django pass") is True
+
+
+class TestAdminModel:
+    def test_admin_model(self, one_admin_user):
+        assert isinstance(one_admin_user, models.Admin)
+        assert one_admin_user.type == models.User.Type.ADMIN
+        assert one_admin_user.is_staff and one_admin_user.is_superuser
+
+
+class TestStudentModel:
+    @pytest.mark.django_db
+    def test_student_model(self, one_student_user):
+        assert isinstance(one_student_user, models.Student)
+        assert one_student_user.type == models.User.Type.STUDENT
+        assert not (one_student_user.is_staff or one_student_user.is_superuser)
+
+
+class TestTeacherModel:
+    @pytest.mark.django_db
+    def test_teacher_model(self, one_teacher_user):
+        assert isinstance(one_teacher_user, models.Teacher)
+        assert one_teacher_user.type == models.User.Type.TEACHER
