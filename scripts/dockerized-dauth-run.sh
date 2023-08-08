a41e0a6af16e0d4cc43afed6770f8970af3a2bf5
nicola <nicolaibrahim969@gmail.com>
2023-08-08 15:18:35 +0300

    Ensure generating SIGNING_KEY for jwt in docker

--- scripts/dockerized-dauth-run.sh
+++ scripts/dockerized-dauth-run.sh
@@ -5,6 +5,18 @@ source .venv/bin/activate
 
 RUN_MANAGE_PY='python manage.py'
 
+echo "----- Generate JWT siging key ------"
+# Create the .keys folder
+mkdir ./config/settings/.keys
+
+# Check if the file exists
+if [ -f ./config/settings/.keys/jwtHS256.key ]; then
+  echo "The key file already exists. No new key is generated."
+  exit 1
+else
+  openssl rand -base64 32 > ./config/settings/.keys/jwtHS256.key
+fi
+
 echo "----- Collect static files ------"
 $RUN_MANAGE_PY collectstatic --noinput
 
